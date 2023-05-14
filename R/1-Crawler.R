f498a19a8ef61b99141c422a00e2dffc1235ab0c
IraitzM <iraitzm@gmail.com>
2023-05-14 17:10:18 +0000

    Crawler added

---
+++ R/1-Crawler.R
@@ -0,0 +1,75 @@
+# author: Iraitz Montalban
+# date: May, 2023
+
+rm(list = ls())
+cat("\014")
+
+# UTF-8 as the main encoding
+options(encoding = "utf-8")
+
+# Package management
+#install.packages("pacman")
+library("pacman")
+
+# Install and load rvest
+pacman::p_load("rvest")
+pacman::p_load(devtools)
+pacman::p_load(dplyr)
+pacman::p_load(RSelenium)
+pacman::p_load(wdman)
+pacman::p_load(xml2)
+pacman::p_load(tidyverse)
+
+# Let's open a phantom browser
+server <- phantomjs(port=5010L)
+remDr <- remoteDriver(browserName = "phantomjs", port=5010L)
+remDr$open()
+
+# We will navigate to our target website
+remDr$navigate("http://www.imdb.com/title/tt1490017/")
+remDr$screenshot(display = TRUE) 
+
+# Get web page HTML code
+pagina <- remDr$getPageSource()[[1]]
+
+# We will select the comments
+resenas <- remDr$findElement(using = 'css selector', 
+                             "#__next > main > div > section.ipc-page-background.ipc-page-background--base.sc-c7f03a63-0.kUbSjY > section > div:nth-child(4) > section > section > div.sc-10602b09-2.jzJCdt > div.sc-10602b09-10.TKmtG > div.sc-10602b09-5.kBrxsq > ul > li:nth-child(1) > a > span > span.label")
+# And click on those
+resenas$clickElement()
+
+# Let's check it out
+remDr$screenshot(display = TRUE) 
+
+# We can navigate as if a human was interacting with the site
+remDr$goBack()
+remDr$screenshot(display = TRUE) 
+remDr$goForward()
+remDr$screenshot(display = TRUE) 
+
+# Let's get the comments
+comments <-xml2::read_html(remDr$getPageSource()[[1]]) %>% # Extraemos el c贸digo HTML
+  html_nodes(xpath = '//*[@id="main"]/section/div[2]/div[2]') %>% html_text()
+comments
+
+# Let's switch Amazon now
+remDr$navigate("https://www.amazon.es/")
+remDr$screenshot(display = TRUE) 
+
+# We will select the search box
+input <- remDr$findElement(using = 'css selector', "#twotabsearchtextbox")
+
+# And do a targheted search
+input$sendKeysToElement(list("Laptop Computer"))
+btn <- remDr$findElement(using = 'css selector',"#nav-search-submit-button")
+btn$clickElement() # Click on search
+
+# See the outcome
+remDr$screenshot(display = TRUE)
+
+# We can check those elements programatically to track if they change prices, for example
+pagina<-remDr$getCurrentUrl()[[1]][1]%>% read_html()
+
+# Let's close the connection
+remDr$close() 
+server$stop()
