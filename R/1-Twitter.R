150776e2cb1e76fb3a0cab13188bc51cec673d4f
IraitzM <iraitzm@gmail.com>
2023-05-21 18:22:39 +0200

    Token retrieval and text cleaning

--- R/1-Twitter.R
+++ R/1-Twitter.R
@@ -11,26 +11,51 @@ options(encoding = "utf-8")
 #install.packages("pacman")
 library("pacman")
 
-pacman::p_load(httr)
+pacman::p_load(httr) # REST functions
 
-# Remember to obtain you TOKEN from the Developer porta
-# > https://developer.twitter.com/en/docs/twitter-api/tweets/search/introduction
+# Remember to obtain you TOKEN from the Developer portal
 # > https://developer.twitter.com/en/docs/tutorials/step-by-step-guide-to-making-your-first-request-to-the-twitter-api-v2
-
-# Token
-token <- '<YOUR TOKEN>'
 
 # API
 api <- 'https://api.twitter.com/'
-endpoint <- '2/tweets/search/recent'
 
-# Parameters
-query <- 'Eurovision'
+# Getting the token automatically
+# > https://developer.twitter.com/en/docs/authentication/api-reference/token
+api_key <- '<API_KEY>'
+api_secret <- '<API_SECRET'
+
+# POST oauth2
+endpoint <- 'oauth2/token'
+parameters <- 'grant_type=client_credentials'
+
+base_url <- paste0(api, endpoint)
+base_url <- paste0(base_url, '?')
+base_url <- paste0(base_url, parameters)
+base_url
+
+user_pass <- paste0(utils::URLdecode(api_key),":")
+user_pass <- paste0(user_pass, utils::URLdecode(api_secret))
+key <- paste0('Basic ', base64(user_pass))
+
+r <- POST(base_url, 
+         add_headers(
+           "Authorization" = key,
+           "Content-Type" = 'application/x-www-form-urlencoded;charset=UTF-8'))
+r$status_code
+response <- content(r, as = "text", encoding = "UTF-8")
+token <- rjson::fromJSON(response)$access_token
+
+### Search
+# > https://developer.twitter.com/en/docs/twitter-api/tweets/search/introduction
+
+#Search enpoint
+endpoint <- '2/tweets/search/recent'
+query <- 'elecciones'
 
 # Auth header
 key <- paste0('Bearer ', token)
 
-# GET
+# Base URL
 base_url <- paste0(api, endpoint)
 base_url <- paste0(base_url, '?query=')
 base_url <- paste0(base_url, query)
@@ -43,5 +68,34 @@ r$status_code
 response <- content(r, as = "text", encoding = "UTF-8")
 response
 
-tweets <- fromJSON(response)
-tweets$data
+# Get structured data
+tweets <- fromJSON(response)$data
+tweet_df <- do.call(rbind, lapply(tweets, as.data.frame))
+
+# Check it, every time we ask, it changes
+
+# Processing text
+pacman::p_load("wordcloud")
+pacman::p_load("RColorBrewer")
+pacman::p_load("wordcloud2")
+pacman::p_load(tidytext)
+
+# Create a vector containing only the text
+text <- tweet_df$text
+
+# Clean text (regex)
+text <- gsub("https\\S*", "", text) 
+text <- gsub("@\\S*", "", text) 
+text <- gsub("amp", "", text) 
+text <- gsub("[\r\n]", "", text)
+text <- gsub("[[:punct:]]", "", text)
+
+tweets_words <-  tweet_df %>%
+  dplyr::select(text) %>%
+  unnest_tokens(word, text)
+words <- tweets_words %>% dplyr::count(word, sort=TRUE)
+
+# Let's paint the wordcloud
+wordcloud(words = words$word, freq = words$n, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"))
